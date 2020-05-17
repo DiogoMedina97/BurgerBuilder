99077005cd61d668450ef6b64221f515ed05700a
DiogoMedina97 <diogo_medina97@hotmail.com>
2020-05-17 00:53:50 -0300

    Routes added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,28 +88,16 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!')
-        this.setState({ loading: true })
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Diogo Medina',
-                address: {
-                    street: 'Test street 1',
-                    zipCode: '99999',
-                    country: 'Brazil'
-                },
-                email: 'diogo@example.com'
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = []
+        for(let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading:false, purchasing: false })
-            })
-            .catch(error => {
-                this.setState({ loading:false, purchasing: false })
-            })
+        queryParams.push('price=' + this.state.totalPrice)
+        const queryString = queryParams.join('&')
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        })
     }
 
     render() {
