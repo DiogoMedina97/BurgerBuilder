3eb286808bc4aeba34e7e8cc0763cab6fa784887
DiogoMedina97 <diogo_medina97@hotmail.com>
2020-05-28 21:41:46 -0300

    Authentication added

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux'
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
 import ContactData from './ContactData/ContactData'
-import * as actions from '../../store/actions/index'
 
 class Checkout extends Component {
     checkoutCancelledHandler = () => {
@@ -28,7 +27,7 @@ class Checkout extends Component {
                         checkoutContinued={this.checkoutContinuedHandler} />
                     <Route 
                         path={this.props.match.path + '/contact-data'} 
-                        component={ContactData} />)} />
+                        component={ContactData} />
                 </div>
             )
         }
